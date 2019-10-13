bfa03232541bc60023523dc0eecc283bd206324f
iflytang <iflytang@qq.com>
2019-10-14 01:09:53 +0800

    [work] auto to adjust the pktgen rate with adjustable time interval

--- pktgen.sh
+++ pktgen.sh
@@ -1,56 +1,76 @@
 #!/bin/bash
 
-# # change flow file here
-PORT_RATE = 10              # in Gbps
 
 # # flow_A, used for bandwidth monitor, figure 16 (a)
-trace_file = flow_A         # in Gbps, every 1s to change rate, last for 44s, loops: 10, sampling rate: 7.1%
-trace_time = time_A         # in second
+export trace=flow_A         # in Gbps, every 1s to change rate, last for 44s, loops: 10, sampling rate: 7.1%
+export run_time=time_A         # in second
 
 # # flow_B, used for bandwidth monitor, figure 16 (b)
-#trace_file = flow_B         # in Gbps, every 1s to change rate, last for 55s, loops: 10, sampling rate: 8.2%
-#trace_time = time_B         # in second
+#export trace=flow_B         # in Gbps, every 1s to change rate, last for 55s, loops: 10, sampling rate: 8.2%
+#export run_time=time_B         # in second
 
-# # flow_C, used for benchmark, figure 15 (a), scenario 1: 64B, 0.672 Mpps, adjust sampling rate
-#trace_file = flow_C         # in Gbps, every 8s to change rate, last for 30*8=240s, loops: 10
-#trace_time = time_C         # in second
+# # flow_C, used for benchmark, figure 15 (a), scenario 1: 64B, 0.672 Mpps, adjust sampling rate; OR for figure 18 (a)
+#export trace=flow_C         # in Gbps, every 8s to change rate, last for 30*8=240s, loops: 10
+#export run_time=time_C         # in second
 
 # # flow_D, used for benchmark, figure 15 (b), scenario 2: 64B, [0.336, 0.672] Mpps, adjust port by ECMP
-#trace_file = flow_D         # in Gbps, every 8s to change rate, last for 30*8=240s, loops: 10
-#trace_time = time_D         # in second
+#export trace=flow_D         # in Gbps, every 8s to change rate, last for 30*8=240s, loops: 10
+#export run_time=time_D         # in second
 
-trace = ${trace_file} / ${PORT_RATE} * 100   # in percentage (%)
-time_time = ${trace_time}                    # in second
+## pktgen tcp ip and port
+export PKTGEN_IP="127.0.0.1"
+export PKTGEN_PORT=22022
+
+# # pktgen running port and rate
+export PORT_RATE=10              # in Gbps
+export PORT_ID_0='0'
+export PORT_ID_1='1'
+export PERCENTRAGE_FACTOR=10     # convert Gbps to %
+
+## pktgen configuration, change here
+export PORT_ID=${PORT_ID_1}
+export PKT_SIZE=64
+export SRC_IP='10.0.0.1/32'    # must have prefix
+export DST_IP='10.0.0.2'       # cannot have prefix
 
 trace_index=0
 time_index=0
-
 #java --version
-while read line
+
+## read trace
+while read trace_line
 do
-    trace_array[$trace_index]=$line
+    trace_array[$trace_index]=${trace_line}*10    # in percentage
    # echo ${trace_array[$trace_index]}
     let trace_index++
 done < ${trace}
-#echo ${trace_array[0]}
-#echo ${trace_array[1]}
-#echo ${trace_array[2]}
 
-while read b
+## read time
+while read time_line
 do
-    time_array[time_index]=$b
-    let time_index+=1
-done < ${time_time}
+    time_array[time_index]=${time_line}
+    let time_index++
+done < ${run_time}
 
+# set src_ip and dst_ip
+echo "pktgen.set_ipaddr(${PORT_ID}, 'src', '${SRC_IP}')" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
+echo "pktgen.set_ipaddr(${PORT_ID}, 'dst', '${DST_IP}')" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
 
-#echo "pktgen.start ('0')" | socat - TCP4:127.0.0.1:22022
-echo "pktgen.set('0','rate',${trace_array[0]})" | socat - TCP4:127.0.0.1:22022
+# set packet protocol
+echo "pktgen.set_proto(${PORT_ID}, 'tcp')" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
+
+# set packet size
+echo "pktgen.set(${PORT_ID}, 'size', ${PKT_SIZE})" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
+#echo ${PORT_ID}
+
+## start to send packet
+#echo "pktgen.start(${PORT_ID})" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
 #sleep 1s
-echo "pktgen.start('0')" | socat - TCP4:127.0.0.1:22022
-#sleep 1s
 
+## auto to adjust packet-rate
 for ((i=0;i<$trace_index;i++))
 do
-    echo "pktgen.set('0','rate',${trace_array[i]})" | socat - TCP4:127.0.0.1:22022
-    sleep ${time_array[i]}
+    echo "pktgen.set(${PORT_ID},'rate',${trace_array[i]})" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
+    sleep ${time_array[i]}s
+    #sleep 1s
 done
