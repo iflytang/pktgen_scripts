5fef42d5433759bf10f0e07e969552df2c6ab620
iflytang <iflytang@qq.com>
2020-08-07 11:21:12 +0800

    ML_INT: auto send and stop Traffic-test trace

--- pktgen.sh
+++ pktgen.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+## when SIGINT || SIGQUIT || SIGKILL, we should stop pktgen and then quit, with 'trap' command
+trap 'signal_handler; exit' SIGINT SIGQUIT SIGKILL
+function signal_handler() {
+    ## stop to send packet
+    echo -e "\ntsf: the shell script will be stopped, and pktgen stops running ..."
+    echo "pktgen.clr" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}  # clear statistics
+    echo "pktgen.stop(${PORT_ID})" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
+}
+
+# # traffic-001, [160000, 170000] as test set. OFC 2021, pkt_size: 1024
+export trace=Traffic-test-001           # in Gbps, every 1s to change rate.
+                                        # we repeat first second for twice, and collector choose the latter one
+export run_time=Traffic-test-001-time   # in second
 
 # # flow_A, used for bandwidth monitor, figure 16 (a)
 #export trace=flow_A         # in Gbps, every 1s to change rate, last for 44s, loops: 10, sampling rate: 7.1%
@@ -14,8 +27,8 @@
 #export run_time=time_C         # in second
 
 # # flow_D, used for benchmark, figure 15 (b), scenario 2: 64B, 0.672 Gbps, adjust port by ECMP; OR for figure 18 (a)
-export trace=flow_D         # in Gbps, every 8s to change rate, last for 30*8=240s, loops: 10
-export run_time=time_D         # in second
+#export trace=flow_D         # in Gbps, every 8s to change rate, last for 30*8=240s, loops: 10
+#export run_time=time_D         # in second
 
 ## pktgen tcp ip and port
 export PKTGEN_IP="127.0.0.1"
@@ -29,7 +42,7 @@ export PERCENTRAGE_FACTOR=10     # convert Gbps to %
 
 ## pktgen configuration, change here
 export PORT_ID=${PORT_ID_0}
-export PKT_SIZE=64             # flow_A,flow_B:1024B; flow_C,flow_D:64B
+export PKT_SIZE=1024             # flow_A,flow_B:1024B; flow_C,flow_D:64B
 export SRC_IP='10.0.0.1/32'    # must have prefix
 export DST_IP='10.0.0.2'       # cannot have prefix
 
@@ -37,15 +50,15 @@ trace_index=0
 time_index=0
 #java --version
 
-## read trace
+## read trace, unit: Gbps
 while read trace_line
 do
-    trace_array[$trace_index]=${trace_line}*10    # in percentage
+    trace_array[$trace_index]=${trace_line}*10    # in percentage, 10 Gbps * percentage = real_bandwidth
    # echo ${trace_array[$trace_index]}
     let trace_index++
 done < ${trace}
 
-## read time
+## read time, unit: second
 while read time_line
 do
     time_array[time_index]=${time_line}
@@ -68,11 +81,19 @@ echo "pktgen.start(${PORT_ID})" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
 #sleep 1s
 
 ## auto to adjust packet-rate
+STOP_POINTS=650
 for ((i=0;i<$trace_index;i++))
 do
     echo "pktgen.set(${PORT_ID},'rate',${trace_array[i]})" | socat - TCP4:${PKTGEN_IP}:${PKTGEN_PORT}
     sleep ${time_array[i]}s
     #sleep 1s
+
+    if (($i>$STOP_POINTS))
+    then
+        let STOP_POINTS++
+        echo "tsf: stop to send traffic after ${STOP_POINTS} points."
+        break
+    fi
 done
 
 ## stop to send packet
